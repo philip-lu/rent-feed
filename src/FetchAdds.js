0d1ca69712ef034917d4c30c9727b8eb177fa702
Philip Lu <philu@miBook1211.local>
2021-04-28 11:06:27 +0300

    Style fixes

--- src/FetchAdds.js
+++ src/FetchAdds.js
@@ -87,6 +87,7 @@ export default class FetchAdds extends React.Component {
                                     <img className="apartment-card-images-item" src={i} key={i} alt={i}/>
                                 ))}
                            </div>
+                           <p className="price"><span className="price-uah"><strong>{apartment.price_uah} грн.</strong></span><span className="price-usd"> / {apartment.price_usd} usd</span></p>
                            <p>этаж {apartment.floor} из {apartment.floors_total}</p>
                            <p className="description">{apartment.description}</p>
                            <a target="_blank" rel="noreferrer" href={apartment.providers[0].url}>{apartment.providers[0].url}</a>
